23c479943d3c6c21c77274d350c9e3ac71e92c7f
julien <julien.bernard.iphone@gmail.com>
2017-03-03 15:17:04 +0100

    update calendar

--- application/services/opta_collector.py
+++ application/services/opta_collector.py
@@ -2,6 +2,7 @@ import datetime
 
 from nameko.rpc import rpc
 from nameko.timer import timer
+from nameko.events import EventDispatcher
 from pymongo import IndexModel, ASCENDING, DESCENDING
 from nameko_mongodb.database import MongoDatabase
 
@@ -10,28 +11,30 @@ from application.dependencies.opta import OptaDependency
 
 class OptaCollectorService(object):
     name = 'opta_collector'
-    
+
     database = MongoDatabase()
-    
+
     opta = OptaDependency()
-    
+
+    dispatcher = EventDispatcher()
+
     def create_f1_indexes(self):
         indexes = [IndexModel([('id', ASCENDING,)])]
 
         self.database.f1.create_indexes(indexes)
-        
+
     def create_f9_indexes(self):
         indexes = [IndexModel([('match_info.id', ASCENDING,)])]
-        
+
         self.database.f9.create_indexes(indexes)
-    
+
     @rpc
     def add_f1(self, season_id, competition_id):
-        
+
         self.create_f1_indexes()
-        
+
         calendar = self.opta.get_calendar(season_id, competition_id)
-        
+
         self.database.f1.insert_many(calendar)
 
     @rpc
@@ -42,11 +45,30 @@ class OptaCollectorService(object):
         ids = self.database.f1.find({'season_id': season_id, 'competition_id': competition_id})
 
         self._load_f9(ids)
-        
+
     @rpc
     @timer(900)
     def update_f1_f9(self):
-    
+
+        calendars = self.database.f1.aggregate([
+            {
+                "$group": {
+                    "_id": {"season_id": "$season_id", "competition_id": "$competition_id"},
+                }
+            }
+        ])
+
+        for row in calendars:
+            calendar = self.opta.get_calendar(row['_id']['season_id'], row['_id']['competition_id'])
+
+            for game in calendar:
+                self.database.f1.update_one({'id': game['id']},
+                                            {'$set': {
+                                                'date': game['date'],
+                                                'home_id': game['home_id'],
+                                                'away_id': game['away_id']
+                                            }})
+
         self.create_f9_indexes()
 
         now = datetime.datetime.utcnow()
@@ -54,7 +76,7 @@ class OptaCollectorService(object):
         end_date = now + datetime.timedelta(minutes=120)
 
         start_date = now - datetime.timedelta(days=3)
-        
+
         ids = self.database.f1.find({'date': {'$gte': start_date, '$lt': end_date}}, {'id': 1})
 
         self._load_f9(ids)
@@ -78,6 +100,5 @@ class OptaCollectorService(object):
                     'missed_penalties': game['missed_penalties'],
                     'substitutions': game['substitutions'],
                     'team_stats': game['team_stats'],
-                    'player_stats': game['team_stats']
+                    'player_stats': game['player_stats']
                 }}, upsert=True)
-
